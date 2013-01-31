bd3af512593c230d74e8929a8bbe1e1fffc5dad0
Vincent Guglielmi <vincent.guglielmi@gmail.com>
2013-01-31 16:21:04 +0100

    add find and add Methods to JAI.List

--- build/jai.js
+++ build/jai.js
@@ -29,10 +29,43 @@ JAI.Node.prototype = {
 
 JAI.List = function() {
 	this.length = 0;
+	this.content = new Array();
 };
 
 JAI.List.prototype = {
-	constructor: JAI.List
+	constructor: JAI.List,
+
+	add: function(node, x, y) {
+		if (!(node instanceof JAI.Node))
+			throw "parameter is not a JAI.Node";
+
+		x = x == null ? 0 : x;
+		y = y == null ? 0 : y;
+
+		var add = true;
+		var find = this.find(x, y);
+
+		if (find !== false) {
+			add = false;
+			if (node.cost_f < this.content[find][2].cost_f )
+				this.content[find][2] = node;
+		};
+
+		if (add) 
+			this.content.push(new Array(x, y, node));
+	},
+
+	find: function(x, y) {
+		var present = false;
+		this.content.forEach(function(element, index, array) {
+			if (element[0] == x && element[1] == y) {
+				present = index;
+			};
+		});
+
+		return present;
+	}
+	
 };
 
 JAI.Map = function(min_x, max_x, min_y, max_y) {
--- test/unit/core/ListUnitTests.js
+++ test/unit/core/ListUnitTests.js
@@ -1,4 +1,100 @@
 test("new JAI.List SHOULD have a zero length", function() {
 	var list = new JAI.List();
 	equal(list.length, 0);
+	ok(list.content instanceof Array);
+	equal(list.content.length, 0);
+});
+
+test("JAI.List.add SHOULD throw an exception if node is not a JAI.Node", function() {
+	var list = new JAI.List();
+	throws(
+		function() {
+			list.add(null);
+		},
+		/parameter is not a JAI.Node/
+	);
+	
+	throws(
+		function() {
+			list.add(new Object());
+		},
+		/parameter is not a JAI.Node/
+	);
+	
+});
+
+test("JAI.List.add SHOULD add the Node in the content property at default position", function() {
+	var list = new JAI.List();
+	var node = new JAI.Node(10, 25, 'x-1:y2');
+	list.add(node);
+	equal(list.content.length, 1);
+	ok(list.content[0] instanceof Array);
+	equal(list.content[0][0], 0);
+	equal(list.content[0][1], 0);
+	equal(list.content[0][2], node);
+});
+
+test("JAI.List.add SHOULD add the node at the great coordinates", function() {
+	var list = new JAI.List();
+	var node = new JAI.Node(10, 25, 'x-1:y2');
+	list.add(node, 4, -6);
+	equal(list.content.length, 1);
+	ok(list.content[0] instanceof Array);
+	equal(list.content[0][0], 4);
+	equal(list.content[0][1], -6);
+	equal(list.content[0][2], node);
+});
+
+test("JAI.List.add SHOULD NOT add the node if a better node is at the same coordinates", function() {
+	var list = new JAI.List();
+	var node = new JAI.Node(10, 25, 'x-1:y2');
+	var better_node = new JAI.Node(10, 5, 'x2:y1');
+	list.add(better_node);
+	list.add(node);
+	equal(list.content.length, 1);
+	ok(list.content[0] instanceof Array);
+	equal(list.content[0][0], 0);
+	equal(list.content[0][1], 0);
+	equal(list.content[0][2], better_node);
+});
+
+
+test("JAI.List.add SHOULD update the node if a better node is added at the same coordinates", function() {
+	var list = new JAI.List();
+	var node = new JAI.Node(10, 25, 'x-1:y2');
+	var better_node = new JAI.Node(10, 5, 'x2:y1');
+	list.add(node);
+	list.add(better_node);
+	equal(list.content.length, 1);
+	ok(list.content[0] instanceof Array);
+	equal(list.content[0][0], 0);
+	equal(list.content[0][1], 0);
+	equal(list.content[0][2], better_node);
+});
+
+test("JAI.List.find SHOULD return false if no node is present at the examinated coordinates", function() {
+	var list = new JAI.List();
+	var node = new JAI.Node(10, 25, 'x-1:y2');
+	var better_node = new JAI.Node(10, 5, 'x2:y1');
+	list.add(node);
+	list.add(node, 1, 3);
+	list.add(node, -4, 6);
+	list.add(node, -1, -2);
+	ok(!list.find(0, 1));
+	ok(!list.find(2, -4));
+	ok(!list.find(-1, -1));
+});
+
+test("JAI.List.find SHOULD return the index in content array where examinated coordinates corresponding", function() {
+	var list = new JAI.List();
+	var node = new JAI.Node(10, 25, 'x-1:y2');
+	var better_node = new JAI.Node(10, 5, 'x2:y1');
+	list.add(node);
+	list.add(node, 1, 3);
+	list.add(node, -4, 6);
+	list.add(node, -1, -2);
+	equal(list.find(0, 0), 0);
+	equal(list.find(1, 3), 1);
+	equal(list.find(-4, 6), 2);
+	equal(list.find(-1, -2), 3);
 });
